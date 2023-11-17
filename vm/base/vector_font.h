6244cf621321ceef8a1eb3f7a24bac604e088429
Anthony Matarazzo <amatarazzo777@gmail.com>
2023-11-16 18:43:19 -0700

    More vfont vector planning.

--- vm/base/vector_font.h
+++ vm/base/vector_font.h
@@ -64,7 +64,54 @@ struct glyph_t {
 
 	Working with user space, a trinket of standard font names exist, yet naming beyond the 
 	few known from corporate network writing, courier, arial, roman, bookman, san sherif, 
-	gothic, and script.
+	gothic, and script. If we disect the problem from a harvested nature, simply the engine of 
+	vector storage supplies the rendering pipeline together with functions that can utilize 2d 
+	geometry, and work with algorithms that are scientific. With the very few routines already 
+	borrowed, it seems that a mountain of code is gone. Now it is time to add more.
+
+	Looking through the character rendering of various Latin fonts listed on the google page,
+	a program that analyzes the font files extracting the glyph measurements and organizing by 
+	name seems natural. An input of perhaps 1500 font vectors as a base for statisical study of
+	font names. Aligning each glyph and storing in a master database measurements that describe 
+	the rendering differences. This is one form of a database that can be stored or used to organize
+	a feature set for a glyph according to language.
+
+	Expanding on the methods currently in use, most fonts seem to be categorized about specific in width, 
+	reading, Form alteration in sections particular to a letter. The letters different in sans, serif. The
+	font varies on effect of curls, square now, Capital letters, and some curls below the Decender line
+	can be variable control. The signature explicit for each time rendering, single character change for
+	a particular font to take into account a jitter movement. Natural variances in some texts can be 
+	fortifide in also helping focus perhaps. 
+
+	So disecting the characters in language from a meta perspective, one logic alteration area, and 
+	rendering pipeline change. The provisions of knowing the segment paths where the tick forms on the 
+	capital W, a labeling system defines itself in addition to official terms perhaps. As a range of 
+	controlling the sub gymph vector path can be object oriented. 
+	
+	A system of dynamically creating fonts by name is a modern feature set in monochrome color. Yet fonts
+	as a vector system, monochrome, offer the ability to reintroduce the font nameing convention with more
+	pleasing user desire by controlling the strength of twenty types of gylph settings at a various strength 
+	than knowning a name that fits the criteria. The ability to craft the size and utilize a conformat 
+	convention is inviting. Color, and many other new features affect the name of the font. 
+	
+	To provide advanced font features will power the browser. The family of technology in use now is the 
+	true type system, an advanced working font system with every language. The dynamic backbone of the GUI
+	industry. Yet often connecting the emotion, desire, personal or commercial desire of a font comes from 
+	a range board selection of font names, perhaps types of advanced pet names. Users know of the massive 
+	amount of fonts, perhaps they desire less fonts when the font browser appears. So dynamically designing
+	a font with normalized parameters accross the glymph range, characters that have a global contextual 
+	knowledge of the style settings across the board, numerically set once, form their curve path, deformation, 
+	expansion, wire hashes, to mimic the style of font. 
+	
+	for each letter, a large definition will exist that drives the rendering of the character set. Offering
+	that these settings together names a font, a branded level of font texture and effects thought of 
+	as a base for C++ app central usage. Ultimately the variance on types of settings, with a known
+	amount of them as a byte signature, with a byte parameter. Makes font sizes very small, fifty bytes,
+	plus the name. So rather than load vector data as font data, makes the engine very small. As well, effects
+	and mood of characters developed by artists, can now be transfered to other languages through their engine.
+	
+
+
 	
 	 */
 };
