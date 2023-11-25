e5024620a5de028e0ea64cf73f8bd57a9daefa01
Anthony Matarazzo <amatarazzo777@gmail.com>
2023-11-25 13:59:30 -0700

    3d graphics.

---
+++ vm/base/scenegraph/scene_graph.h
@@ -0,0 +1,20 @@
+/*
+
+a software attachement to the video card abstraction layer replacing 
+the existing components such as vulcan and opengl. The capability
+of the software to exist as a llvm instaniated interface to an instance
+of the video card make it possible to provide a better form
+of clipping and capabilities. The extensiveness of the scene graph 
+collection should be base functional parts of the system to incorporate
+into process animation. By placing multiple compiler requires together,
+other forms of shader can be utilized. Yet the focus of the library is
+manufactureing game graphics as a bse system.Easily integrated into 
+display with the text and rendering system. Integration with the UI
+component is also a consideration. Developed text and textures are 
+necessary to transmit from the vfont text system. Beveling data, and 
+lighting data. The system can use the same timeing data nd animation 
+system as the 2d vector. Rate and time changes with events.
+
+
+
+*/
